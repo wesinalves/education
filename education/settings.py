e8ad88b3f607e88d942b4e44bc06f97f963f0b56
Wesin Ribeiro <wesinec@gmail.com>
2022-10-29 23:26:30 -0300

    fix bootstrap configuration

--- education/settings.py
+++ education/settings.py
@@ -123,6 +123,20 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = 'static/'
 
+BOOTSTRAP5 = {
+    "css_url": {
+        "href": "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
+        "integrity": "sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi",
+        "crossorigin": "anonymous",
+    },
+    # The complete URL to the Bootstrap JavaScript file
+    "javascript_url": {
+        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js",
+        "integrity": "sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3",
+        "crossorigin": "anonymous",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
